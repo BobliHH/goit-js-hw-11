fafefab3d06a6bce50956a6be613b30d4ac5e2e6
=Andrei <balasca.andrei@gmail.com>
2023-05-29 17:02:13 +0300

    added some styles and imports

--- src/index.js
+++ src/index.js
@@ -1,55 +1,17 @@
 import './css/styles.css';
 import notiflix from 'notiflix';
-import lodash from 'lodash';
-import debounce from 'lodash/debounce';
-import { fetchCountries } from './fetchCountries';
+import axios from 'axios';
+// Descris în documentație
+import SimpleLightbox from "simplelightbox";
+// Import suplimentar de stil
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-const DEBOUNCE_DELAY = 300;
-const searchBox = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
 
-searchBox.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
 
-function onInputSearch(e) {
-  e.preventDefault(e);
-  const SearchForContry = e.target.value.trim();
 
-  if (!SearchForContry) {
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
-    return;
-  }
 
-  fetchCountries(SearchForContry)
-          .then(result => {
-              if (result.length > 10) {
-                  notiflix.Notify.info(
-                    'Too many matches found. Please, enter a more specific name.'
-                  );
-                  return;
-              }
-              renderedCountries(result);
-          })
-          .catch(error => {
-              countryList.innerHTML = '';
-              countryInfo.innerHTML = '';
-              notiflix.Notify.failure(
-                'Oops, there is no country with that name'
-              );
-          })
-};
 
-function renderedCountries(result) {
-    const inputLength = result.length;
+const body = document.querySelector('body');
 
-    if (inputLength === 1) {
-        countryList.innerHTML = "";
-        //return card with winformation
-    }
+body.style.backgroundColor = 'rgb(0, 0, 168)';
 
-    if (inputLength > 1) {
-        countryInfo.innerHTML = "";
-        //return card with winformation
-    }
-};
