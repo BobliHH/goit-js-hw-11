d73908b9aadb41825d6d03d6dad95eebcc993e5e
=Andrei <balasca.andrei@gmail.com>
2023-06-01 19:02:13 +0300

    created gallery

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
 import './css/styles.css';
 import notiflix from 'notiflix';
-import axios from 'axios';
 // Descris în documentație
 import simpleLightbox from "simplelightbox";
 // Import suplimentar de stil
 import "simplelightbox/dist/simple-lightbox.min.css";
-
-
-
-
-
+import { fetchImages } from './js/fetchImages';
+import { renderGallery } from './js/renderImages';
 
 const body = document.querySelector('body');
-
 body.style.backgroundColor = 'rgb(0, 0, 168)';
 
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+let query = '';
+let page = 1;
+let simpleLightBox;
+const perPage = 40;
+
+searchForm.addEventListener('submit', onSearchForm);
+
+function onSearchForm(e) {
+  e.preventDefault();
+  page = 1;
+  query = e.currentTarget.searchQuery.value.trim();
+  gallery.innerHTML = '';
+  
+  if (query === '') {
+    alertNoEmptySearch();
+    return;
+  }
+
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      if (data.totalHits === 0) {
+        alertNoImagesFound();
+      } else {
+        renderGallery(data.hits);
+        simpleLightBox = new simpleLightbox('.gallery a').refresh();
+        alertImagesFound(data);
+      }
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+      searchForm.reset();
+    });
+}
+
+function alertImagesFound(data) {
+  notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+}
+
+function alertNoEmptySearch() {
+  notiflix.Notify.failure(
+    'The search string cannot be empty. Please specify your search query.'
+  );
+}
+
+function alertNoImagesFound() {
+  notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+}
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export { fetchImages };
 
-this.$axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 const AUTH_KEY = '36858023-bcc8002212b119e45a3b53208';
 
 async function fetchImages(query, page, perPage) {
   const response = await axios.get(
-    `?key=${AUTH_KEY}&q=${query}&image_type=all&page=${page}&per_page=${perPage}`
+    `?key=${AUTH_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
   );
   return response;
 }
